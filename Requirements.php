01829b18b45abf3c3e74306ef1e1ea74bbdb6975
Miguel Elias dos Santos <miguel.elias@hi-midia.com>
2013-10-29 16:43:55 -0200

    Fixed composer.json

--- Requirements.php
+++ Requirements.php
@@ -12,12 +12,12 @@ class Requirements implements PluginInterface
     public function activate (Composer $Composer, IOInterface $IO) {
         $IO->write('Installing handlers for ExtendedRequirements');
         
-        $Composer->getConfig()->merge(array(
-            'scripts' => array(
-                "post-install-cmd" => "migueleliasweb\\extended-requirements\\Scripts::postCmd",
-                "post-update-cmd" => "migueleliasweb\\extended-requirements\\Scripts::postCmd"
-            )
-        ));
+//        $Composer->getConfig()->merge(array(
+//            'scripts' => array(
+//                "post-install-cmd" => "migueleliasweb\\extended-requirements\\Scripts::postCmd",
+//                "post-update-cmd" => "migueleliasweb\\extended-requirements\\Scripts::postCmd"
+//            )
+//        ));
     }
 
 }
