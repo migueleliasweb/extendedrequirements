fad36287c08bdcdf7855721fb58a607e5f595710
Miguel Elias dos Santos <miguel.elias@hi-midia.com>
2013-10-31 10:48:58 -0200

    Many changes

--- Scripts.php
+++ Scripts.php
@@ -9,56 +9,83 @@ class Scripts
 
     const CUSTOM_EXTRA_KEY = 'extended-script-requirements';
     
-    private $AllowedConstraints = array ('<', 'lt', '<=', 'le', '>', 'gt', '>=', 'ge', '==', '=', 'eq', '!=', '<>', 'ne');
-
+    public static $AllowedConstraints = array ('<', 'lt', '<=', 'le', '>', 'gt', '>=', 'ge', '==', '=', 'eq', '!=', '<>', 'ne');
+    
     public function getAllowedConstraints () {
         return $this->AllowedConstraints;
     }
-    
-    public static function postCmd (Event $Event) {
-        $ExtraConfigs = $Event->getComposer()->getConfig()->get('extra');
 
-        foreach ($ExtraConfigs[CUSTOM_EXTRA_KEY] as $ExtraRequirement) {
-            $Requirement = ucfirst(key($ExtraRequirement));
-            $RequiredVersion = ucfirst(key($ExtraRequirement));
+        
+    public static function postCmd (Event $Event) {
+        $ExtraConfigs = $Event->getComposer()->getConfig()->get('extra');//PHP 5.3 ='(
+        
+        foreach ($ExtraConfigs[CUSTOM_EXTRA_KEY] as $Requirement => $Info) {
+            //Ex: getPythonVersion(), getRubyVersion(), getJavaVersion
             $MethodName = 'get'.$Requirement.'Version';
             
-            if (method_exists($this, $MethodName)) {
+            if (method_exists(Scripts, $MethodName)) {
+                $RequiredVersion = $Info;
                 $InstalledVersion = Script::$MethodName();
-            } elseif (class_exists($Requirement)) {
-                $InstalledVersion = new $Requirement();
-            } elseif (file_exists($Requirement) AND is_executable($Requirement)) {
-                $InstalledVersion = shell_exec($Requirement);
-            } else {
-                $InstalledVersion = false;
-            }
-            
-            try {
-                if ($this->compareVersionRequirements($RequiredVersion, $InstalledVersion)) {
-                    $Event->getIo()->write('Requirement verification succeeded for: ' . $Requirement);
+            } elseif (is_array($RequiredVersion)) {//For custom comparisons
+                $RequiredVersion = $Info['RequiredVersion'];
+                        
+                if (file_exists($Info['InstalledVersion']) AND is_executable($Info['InstalledVersion'])) {
+                    $InstalledVersion = shell_exec($Info['InstalledVersion']);
                 } else {
                     $Event->getIo()->write('Requirement verification failed for: ' . $Requirement);
-
-                    if ($InstalledVersion !== false) {
-                        $Event->getIo()->write('--- Required version: '.$RequiredVersion.', installed version: '.$InstalledVersion);
-                    } else {
-                        $Event->getIo()->write('--- File not found or not enough permission to execute it.');
-                    }
-                }    
-            } catch (Exception $E) {
+                    $Event->getIo()->write('--- File not found or not enough permission to execute it.');
+                    continue;
+                }
+            }
+            
+            if (Scripts::compareVersionRequirements($InstalledVersion, $RequiredVersion)) {
+                $Event->getIo()->write('Requirement verification succeeded for: ' . $Requirement);
+            } else {
                 $Event->getIo()->write('Requirement verification failed for: ' . $Requirement);
-                $Event->getIo()->write('--- Could not compare versions.');
+                $Event->getIo()->write('--- Required version: '.$RequiredVersion.', installed version: '.$InstalledVersion);
             }
         }
     }
     
     /**
-     * @param string $InstalledVersion (Cannot contain operators) Ex: 5.9.3 or 7.0.1 or 2.5.1
-     * @param string $RequiredVersion (Can contain operators) Ex: >=1.2.3 or <12.44 or 5.7.1
-     * @throws Exception Throws exception if version could not be compared correctly 
-     * @return boolean Returns wether the required version was fullfilled
+     * Defaults to ">="
+     * @param string $RequiredVersion Ex: >=5.3.1, 0.0.1, =8.1
+     * @return string The operator string to be use on version_compare().
      */
-    private static function compareVersionRequirements ($InstalledVersion, $RequiredVersion) {
+    public static function stripVersionOperator ($RequiredVersion) {
+        if (in_array($RequiredVersion{0}, $this->getAllowedConstraints())) {
+            return $RequiredVersion{0};
+        } elseif (in_array(substr($RequiredVersion, 0, 2), $this->getAllowedConstraints())) {
+            return substr($RequiredVersion, 0, 2);
+        } else {
+            return ">=";
+        }
+    }
+    
+    /**
+     * 
+     * @throws Exception Throws excaption if version is not correctly formed
+     * @param type $RequiredVersion
+     * @return string The standardized "version" of $RequiredVersion
+     */
+    public static function stripRequiredVersion ($RequiredVersion) {
+        if (in_array($RequiredVersion{0}, $this->getAllowedConstraints())) {
+            return substr($RequiredVersion, 1);
+        } elseif (in_array(substr($RequiredVersion, 0, 2), $this->getAllowedConstraints())) {
+            return substr($RequiredVersion, 2);
+        } else {
+            throw new Exception('The version '.$RequiredVersion.' is off standards.');
+        }
+    }
+
+
+    /**
+     * @param string $InstalledVersion (Cannot contain operators) Ex: 5.9.3 or 7.0.1 or 2.5.1
+     * @param string $RequiredVersion Ex: >=1.2.3 or <12.44 or =5.7.1 or >1.1 or =0.9.1
+     * @throws Exception Throws exception if version could not be compared correctly 
+     * @return boolean True whether the requirements were fulfilled
+     */
+    public static function compareVersionRequirements ($InstalledVersion, $RequiredVersion) {
         if (in_array($RequiredVersion{0}, $this->getAllowedConstraints())) {
             $Operator = $RequiredVersion{0};
             $RequiredVersion = substr($RequiredVersion, 1);
@@ -68,37 +95,37 @@ class Scripts
         } elseif ($InstalledVersion === $RequiredVersion) {
             return true;//Could't say I didn't try
         } else {
-            throw new Exception('Could not compare versions.');
+            $Operator = '>=';//Ex: >=5.3.5, >=7, >=0.0.1
         }
         
         return version_compare($InstalledVersion, $RequiredVersion, $Operator);
     }
 
-    private static function getPythonVersion () {
+    public static function getPythonVersion () {
         
     }
 
-    private static function getRubyVersion () {
+    public static function getRubyVersion () {
         
     }
 
-    private static function getJavaVersion () {
+    public static function getJavaVersion () {
         
     }
 
-    private static function getMongoDbVersion () {
+    public static function getMongoDbVersion () {
         
     }
 
-    private static function getPostgreSqlVersion () {
+    public static function getPostgreSqlVersion () {
         
     }
 
-    private static function getMySqlVersion () {
+    public static function getMySqlVersion () {
         
     }
 
-    private static function getNodeJsVersion () {
+    public static function getNodeJsVersion () {
         
     }
 
